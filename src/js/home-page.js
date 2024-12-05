60a406b49741ac815c4c1d64d533052d2e016731
NatalieTesam <nataliekay1023@gmail.com>
2024-12-05 09:58:27 -0700

    removed comments

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,23 +1,5 @@
 const url = "https://gutendex.com/books/"
 
-// async function getJson(endpoint) {
-//     // replace this with your actual key
-//     // construct the url: baseUrl + endpoint + parameters
-//     const url = baseUrl + endpoint;
-//     // set the options. The important one here is the X-Api-Key
-//     const options = {
-//       method: "GET"
-//     }
-//     // make the request
-//     const response = await fetch(url, options)
-//     const data = await response.json()
-//     console.log(data)
-//     return data
-//   }
-
-  
-//   getJson("books/");
-
 async function getData() {
     try {
       const response = await fetch(url);
