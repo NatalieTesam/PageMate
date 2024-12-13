841341dec43d847cba7a3e0b6f8ce79499abd23d
NatalieTesam <nataliekay1023@gmail.com>
2024-12-13 09:32:04 -0700

    exported functions

--- src/js/modules/bookList.js
+++ src/js/modules/bookList.js
@@ -1,19 +1,18 @@
-
-const myList = {saved: [1, 2, 3], read: [1, 2, 3]}
+// list format: const myList = {saved: [1, 2, 3], read: [1, 2, 3]}
 
 // parses the string from local storage and returns a json value
-function getBookList() {
+export function getBookList() {
     return JSON.parse(localStorage.getItem("bookList"));
 }
 
 // turns the json back into a string and sets in local storage
-function setBookList(newList) {
+export function setBookList(newList) {
     localStorage.setItem("booklist", JSON.stringify(newList));
 }
 
 // checks if a book is already listed in the list, pushes it to the list, and then returns 
 // the booklist to local storage to update the value
-function addBook(id, category) {
+export function addBook(id, category) {
     const booklist = getBookList();
     if (category == "saved" & !booklist.saved.includes(id)) {
             booklist.saved.push(id);
@@ -29,7 +28,7 @@ function addBook(id, category) {
 
 // uses a book id and a category string to check if the book is listed in the category then 
 // returns a new list without it and sets the local storage to that new list
-function removeBook(id, category) {
+export function removeBook(id, category) {
     const booklist = getBookList();
     if (category == "saved" & booklist.saved.includes(id)) {
         const newList = booklist.saved.filter(item => item !== id);
