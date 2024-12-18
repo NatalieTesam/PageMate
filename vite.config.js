432eb13c86be55c2fdd5b82b9603a2965a2cf5c9
jmchargue54 <jmchargue54@gmail.com>
2024-12-17 23:10:33 -0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from "path";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -8,7 +8,9 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        search: resolve(__dirname, "src/search.html"),
+        list: resolve(__dirname, "src/list.html")
       }
     }
   }
